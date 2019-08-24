7ba5bbc1d8f51e9239ac4fdb75170fa95738e685
zhucj <janforp@163.com>
2019-08-24 13:39:42 +0800

    deadLock死锁

---
+++ src/main/java/com/janita/vm/vmbetter/controller/DeadLockController.java
@@ -0,0 +1,52 @@
+package com.janita.vm.vmbetter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 类说明：
+ *
+ * @author zhucj
+ * @since 2019-08-24 - 13:32
+ */
+@RestController
+@RequestMapping("/lock")
+public class DeadLockController {
+
+    private Object lock1 = new Object();
+
+    private Object lock2 = new Object();
+
+    /**
+     * 死锁
+     */
+    @GetMapping("/deadlock")
+    public String deadlock() {
+        new Thread(() -> {
+            synchronized (lock1) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    //
+                }
+                synchronized (lock2) {
+                    System.out.println("Thread1 over");
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            synchronized (lock2) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    //
+                }
+                synchronized (lock1) {
+                    System.out.println("Thread2 over");
+                }
+            }
+        }).start();
+        return "deadlock";
+    }
+}
