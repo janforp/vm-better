d1fa14c632d2702acb8cf02518ffbac3580af83d
zhucj <janforp@163.com>
2019-08-24 16:07:42 +0800

    btrace的使用 -- PrintJinfo

--- src/main/java/com/janita/vm/vmbetter/controller/btrace/PrintJinfo.java
+++ src/main/java/com/janita/vm/vmbetter/controller/btrace/PrintJinfo.java
@@ -3,15 +3,22 @@ package com.janita.vm.vmbetter.controller.btrace;
 import com.sun.btrace.BTraceUtils;
 import com.sun.btrace.annotations.BTrace;
 
+/**
+ * 拦截异常
+ *
+ * @author janita
+ */
 @BTrace
 public class PrintJinfo {
+
+    //类似jinfo命令
     static {
-    	BTraceUtils.println("System Properties:");
-    	BTraceUtils.printProperties();
-    	BTraceUtils.println("VM Flags:");
-    	BTraceUtils.printVmArguments();
-    	BTraceUtils.println("OS Enviroment:");
-    	BTraceUtils.printEnv();
-    	BTraceUtils.exit(0);
+        BTraceUtils.println("System Properties:");
+        BTraceUtils.printProperties();
+        BTraceUtils.println("VM Flags:");
+        BTraceUtils.printVmArguments();
+        BTraceUtils.println("OS Enviroment:");
+        BTraceUtils.printEnv();
+        BTraceUtils.exit(0);
     }
 }
