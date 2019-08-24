8dca2434737d81025a38fa03c6926989a5201a2e
zhucj <janforp@163.com>
2019-08-24 14:49:44 +0800

    btrace

--- src/main/java/com/janita/vm/vmbetter/controller/btrace/BtraceController.java
+++ src/main/java/com/janita/vm/vmbetter/controller/btrace/BtraceController.java
@@ -1,6 +1,7 @@
 package com.janita.vm.vmbetter.controller.btrace;
 
 import com.janita.vm.vmbetter.dto.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,32 +16,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/btrace")
 public class BtraceController {
 
-    @RequestMapping("/arg1")
+    @GetMapping("/arg1")
     public String arg1(@RequestParam("name") String name) {
         return "hello," + name;
     }
 
-    @RequestMapping("/arg2")
+    @GetMapping("/arg2")
     public User arg2(User user) {
         return user;
     }
 
-    @RequestMapping("/constructor")
+    @GetMapping("/constructor")
     public User constructor(User user) {
         return user;
     }
 
-    @RequestMapping("/same1")
+    @GetMapping("/same1")
     public String same(@RequestParam("name") String name) {
         return "hello," + name;
     }
 
-    @RequestMapping("/same2")
+    @GetMapping("/same2")
     public String same(@RequestParam("name") String name, @RequestParam("id") int id) {
         return "hello," + name + "," + id;
     }
 
-    @RequestMapping("/exception")
+    @GetMapping("/exception")
     public String exception() {
         try {
             System.out.println("start...");
